545a05f8ba543ec4ee4dd6781b1eb828cc3066c4
RAnami156 <romaandreev414@gmail.com>
2024-11-30 12:40:27 +0100

    powerlift 0.1

---
+++ Powerlifting/beanch/JS.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
